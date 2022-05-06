b52cc45ec63548d210f19df7a5db62b0faf55734
Vin Gabriel Fillon <53989590+Shinixagami@users.noreply.github.com>
2022-05-06 16:02:40 +0000

    +added wallet connect

--- src/App.jsx
+++ src/App.jsx
@@ -36,10 +36,35 @@ const App = () => {
     }
   }
 
+  
+
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
 
+   
+  
   return (
     <div className="mainContainer">
       <div className="dataContainer">
@@ -54,6 +79,23 @@ const App = () => {
         <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+         {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+
+
+        
+
+
+        
+
+        
       </div>
     </div>
     );
